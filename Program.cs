54cf17ae63696081c8bb6cedc04603105c0d53d7
Станислав Успенский <10000335+badhitman@users.noreply.github.com>
2019-02-20 19:51:54 +0300

    ~

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿////////////////////////////////////////////////
-// © https://github.com/badhitman 
+// © https://github.com/badhitman - Telegram @fakegov 
 ////////////////////////////////////////////////
 using MultiTool.LibraryLog;
 using MultiTool.LibraryParserIncomingArguments;
